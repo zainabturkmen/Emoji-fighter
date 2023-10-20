deb77d8a1b8e26e743c0684957e657604688374f
Kang song <kangsong299@gmail.com>
2023-10-20 09:45:40 +0430

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+let fighters = ["🐉", "🐥", "🐊","💩", "🦍", "🐢", "🐩", "🦭", "🦀", "🐝", "🤖", "🐘", "🐸", "🕷","🐆", "🦕", "🦁"]
+
+let stageEl = document.getElementById("stage")
+let fightButton = document.getElementById("fightButton")
+
+fightButton.addEventListener("click", function() {
+    // Challenge:
+    // When the user clicks on the "Pick Fighters" button, pick two random 
+    // emoji fighters and display them as i.e. "🦀 vs 🐢" in the "stage" <div>.
+    let randomIndexOne = Math.floor( Math.random() * fighters.length )
+    let randomIndexTwo = Math.floor( Math.random() * fighters.length )
+    stageEl.textContent = fighters[randomIndexOne] + " vs " + fighters[randomIndexTwo]
+})
