6865c1d17e0431c3771e7f088c7205dda232e752
Kang song <kangsong299@gmail.com>
2023-10-23 08:53:17 +0430

    changing the div name

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-let fighters = ["🐉", "🐥", "🐊","💩", "🦍", "🐢", "🐩", "🦭", "🦀", "🐝", "🤖", "🐘", "🐸", "🕷","🐆", "🦕", "🦁"]
+let fighters = ["🐉", "🐥", "🐊", "💩", "🦍", "🐢", "🐩", "🦭", "🦀", "🐝", "🤖", "🐘", "🐸", "🕷", "🐆", "🦕", "🦁"]
 
-let stageEl = document.getElementById("stage")
+let stageEl = document.getElementById("stage1")
 let fightButton = document.getElementById("fightButton")
 
-fightButton.addEventListener("click", function() {
+fightButton.addEventListener("click", function () {
     // Challenge:
     // When the user clicks on the "Pick Fighters" button, pick two random 
     // emoji fighters and display them as i.e. "🦀 vs 🐢" in the "stage" <div>.
-    let randomIndexOne = Math.floor( Math.random() * fighters.length )
-    let randomIndexTwo = Math.floor( Math.random() * fighters.length )
+    let randomIndexOne = Math.floor(Math.random() * fighters.length)
+    let randomIndexTwo = Math.floor(Math.random() * fighters.length)
     stageEl.textContent = fighters[randomIndexOne] + " vs " + fighters[randomIndexTwo]
 })
